778fae27c5c1ec8eafe696457c896898c06e4165
Vinicius Mazon <vnpmazon@gmail.com>
2020-10-10 09:12:44 -0300

    feat(web): Adds TextArea Component

---
+++ web/src/components/Form/TextArea/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+
+  > label {
+    font: 500 1.6rem 'Montserrat';
+    color: var(--text);
+    margin-bottom: 0.7rem;
+  }
+  > textarea {
+    height: 16rem;
+    width: 100%;
+    padding: 2.3rem 3.8rem;
+    border: 0.1rem solid var(--border);
+    border-radius: 0.8rem;
+    outline: 0;
+    resize: none;
+    &:focus {
+      border-color: var(--green);
+    }
+  }
+  > span {
+    text-align: end;
+    margin-top: 0.5rem;
+    width: 100%;
+    color: var(--status-unavailable);
+  }
+`;
