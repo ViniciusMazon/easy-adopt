e87631812b2926539a5f83b30cd09f64330a6a11
Vinicius Mazon <vnpmazon@gmail.com>
2020-11-02 18:39:12 -0300

    fix(api): Fixes migrations lock during testing

--- api/tests/integration/adoptionRequests.spec.js
+++ api/tests/integration/adoptionRequests.spec.js
@@ -4,42 +4,51 @@ const app = require('../../src/app');
 
 describe('Animals', () => {
   beforeEach(async () => {
-    await db.migrate.rollback();
     await db.migrate.latest();
     await db.seed.run();
   });
 
   afterEach(async () => {
+    await db.migrate.rollback();
+  });
+
+  afterAll(async () => {
     await db.destroy();
   });
 
-  it('POST / Should create a new adoption request', async () => {
-    const response = await request(app).post('/adoption-request').send({
-      residence_type: 'Casa',
-      adults_home: '2',
-      children_home: '1',
-      smokers_home: 'Não',
-      adopted_before: 'Não',
-      other_animals: 'Não',
-      sick_animals: 'Não',
-      aware_cost: 'Sim',
-      why_want_adopt: 'Quero ajudar a melhorar a vida dos animais',
-      animal_id: '1bc2',
-      tutor_id: 'hrg123',
+  describe('POST', () => {
+    it('Should create a new adoption request', async () => {
+      const response = await request(app).post('/adoption-request').send({
+        residence_type: 'Casa',
+        adults_home: '2',
+        children_home: '1',
+        smokers_home: 'Não',
+        adopted_before: 'Não',
+        other_animals: 'Não',
+        sick_animals: 'Não',
+        aware_cost: 'Sim',
+        why_want_adopt: 'Quero ajudar a melhorar a vida dos animais',
+        animal_id: '1bc2',
+        tutor_id: 'hrg123',
+      });
+
+      expect(response.statusCode).toEqual(201);
     });
-
-    expect(response.statusCode).toEqual(201);
   });
 
-  it('INDEX / Should list all adoption requests', async () => {
-    const response = await request(app).get('/adoption-request');
+  describe('INDEX', () => {
+    it('Should list all adoption requests', async () => {
+      const response = await request(app).get('/adoption-request');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('SHOW / Should list an adoption request', async () => {
-    const response = await request(app).get('/adoption-request/fdas878');
+  describe('SHOW', () => {
+    it('Should list an adoption request', async () => {
+      const response = await request(app).get('/adoption-request/fdas878');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 });
--- api/tests/integration/animals.spec.js
+++ api/tests/integration/animals.spec.js
@@ -4,62 +4,75 @@ const app = require('../../src/app');
 
 describe('Animals', () => {
   beforeEach(async () => {
-    await db.migrate.rollback();
     await db.migrate.latest();
     await db.seed.run();
   });
 
   afterEach(async () => {
+    await db.migrate.rollback();
+  });
+
+  afterAll(async () => {
     await db.destroy();
   });
 
-  it('POST / Should register a new animal', async () => {
-    const response = await request(app)
-      .post('/animals')
-      .field('name', 'Mikka')
-      .field('specie', 'Cachorro')
-      .field('gender', 'Fêmea')
-      .field('size', 'Pequeno')
-      .field('age', 'Filhote')
-      .field('status', 'disponível')
-      .attach('images', 'tests/fixtures/a.jpg')
-      .attach('images', 'tests/fixtures/a.jpg')
-      .attach('images', 'tests/fixtures/a.jpg');
+  describe('POST', () => {
+    it('Should register a new animal', async () => {
+      const response = await request(app)
+        .post('/animals')
+        .field('name', 'Mikka')
+        .field('specie', 'Cachorro')
+        .field('gender', 'Fêmea')
+        .field('size', 'Pequeno')
+        .field('age', 'Filhote')
+        .field('status', 'disponível')
+        .attach('images', 'tests/fixtures/a.jpg')
+        .attach('images', 'tests/fixtures/a.jpg')
+        .attach('images', 'tests/fixtures/a.jpg');
 
-    expect(response.body.name).toEqual('Mikka');
+      expect(response.body.name).toEqual('Mikka');
+    });
   });
 
-  it('PUT / Should update a animal', async () => {
-    const response = await request(app)
-      .put('/animals/1bc2')
-      .field('name', 'Mikka')
-      .field('gender', 'Fêmea')
-      .field('specie', 'Cachorro')
-      .field('size', 'Pequeno')
-      .field('age', 'Filhote')
-      .field('status', 'indisponível')
-      .attach('images', 'tests/fixtures/a.jpg')
-      .attach('images', 'tests/fixtures/a.jpg')
-      .attach('images', 'tests/fixtures/a.jpg');
+  describe('PUT', () => {
+    it('Should update a animal', async () => {
+      const response = await request(app)
+        .put('/animals/1bc2')
+        .field('name', 'Mikka')
+        .field('gender', 'Fêmea')
+        .field('specie', 'Cachorro')
+        .field('size', 'Pequeno')
+        .field('age', 'Filhote')
+        .field('status', 'indisponível')
+        .attach('images', 'tests/fixtures/a.jpg')
+        .attach('images', 'tests/fixtures/a.jpg')
+        .attach('images', 'tests/fixtures/a.jpg');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('DELETE / Should delete a animal', async () => {
-    const response = await request(app).delete('/animals/1bc2');
+  describe('DELETE', () => {
+    it('DELETE / Should delete a animal', async () => {
+      const response = await request(app).delete('/animals/1bc2');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('INDEX / Should list all registered animals', async () => {
-    const response = await request(app).get('/animals');
+  describe('INDEX', () => {
+    it('Should list all registered animals', async () => {
+      const response = await request(app).get('/animals');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('SHOW / Should list a registered animal', async () => {
-    const response = await request(app).get('/animals/1bc2');
+  describe('SHOW', () => {
+    it('Should list a registered animal', async () => {
+      const response = await request(app).get('/animals/1bc2');
 
-    expect(response.statusCode).toEqual(200);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 });
