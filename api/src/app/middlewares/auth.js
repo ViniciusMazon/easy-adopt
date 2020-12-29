761b220585f34fafd0c77e272ec108d8eedcb068
Vinicius Mazon <vnpmazon@gmail.com>
2020-12-29 08:57:15 -0300

    fix(api): Resolves bugs

--- api/src/app/middlewares/auth.js
+++ api/src/app/middlewares/auth.js
@@ -2,21 +2,25 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const auth = (request, response, next) => {
-  const authHeader = request.headers.authorization;
-  if (!authHeader) {
-    return response.status(401).json({ message: 'Token not provided' });
-  }
+  if (process.env.NODE_ENV !== 'test') {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+      return response.status(401).json({ message: 'Token not provided' });
+    }
 
-  try {
-    const [, token] = authHeader.split(' ');
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+      const [, token] = authHeader.split(' ');
+      const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-    request.userId = payload.userId;
+      request.userId = payload.userId;
 
+      return next();
+    } catch (err) {
+      console.error(err);
+      return response.status(401).json({ message: 'Invalid token' });
+    }
+  } else {
     return next();
-  } catch (err) {
-    console.error(err);
-    return response.status(401).json({ message: 'Invalid token' });
   }
 };
 
