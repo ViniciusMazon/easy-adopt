be1979e3a3bab0d30b19304adb6559f59490a3c1
Vinicius Mazon <vnpmazon@gmail.com>
2021-01-07 10:55:27 -0300

    memo(mobile): Adds adoption term

--- mobile/src/pages/AdoptionForm/AdoptionTerm.js
+++ mobile/src/pages/AdoptionForm/AdoptionTerm.js
@@ -39,41 +39,34 @@ export default function AdoptionTerm() {
 
       <ScrollView style={styles.termContainer}>
         <Text style={styles.termText}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          facilisis nisl a mauris faucibus, sit amet fringilla tellus
-          consectetur. Proin euismod nisl leo, et tincidunt sapien efficitur
-          quis. Mauris suscipit viverra sem, vitae imperdiet quam euismod et.
-          Cras lobortis libero eget dui porta tempor. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Nullam facilisis nisl a mauris
-          faucibus, sit amet fringilla tellus consectetur. Proin euismod nisl
-          leo, et Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam facilisis nisl a mauris faucibus, sit amet Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Nullam facilisis nisl a mauris
-          faucibus, sit amet fringilla tellus consectetur. Proin euismod nisl
-          leo, et tincidunt sapien efficitur quis. Mauris suscipit viverra sem,
-          vitae imperdiet quam euismod et. Cras lobortis libero eget dui porta
-          tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam facilisis nisl a mauris faucibus, sit amet fringilla tellus
-          consectetur. Proin euismod nisl leo, et Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nullam facilisis nisl a mauris faucibus,
-          sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam facilisis nisl a mauris faucibus, sit amet fringilla tellus
-          consectetur. Proin euismod nisl leo, et tincidunt sapien efficitur
-          quis. Mauris suscipit viverra sem, vitae imperdiet quam euismod et.
-          Cras lobortis libero eget dui porta tempor. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Nullam facilisis nisl a mauris
-          faucibus, sit amet fringilla tellus consectetur. Proin euismod nisl
-          leo, et Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam facilisis nisl a mauris faucibus, sit amet Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Nullam facilisis nisl a mauris
-          faucibus, sit amet fringilla tellus consectetur. Proin euismod nisl
-          leo, et tincidunt sapien efficitur quis. Mauris suscipit viverra sem,
-          vitae imperdiet quam euismod et. Cras lobortis libero eget dui porta
-          tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam facilisis nisl a mauris faucibus, sit amet fringilla tellus
-          consectetur. Proin euismod nisl leo, et Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Nullam facilisis nisl a mauris faucibus,
-          sit amet
+          Ao adotar o animal acima descrito declaro-me apto para assumir a
+          guarda e a responsabilidade sobre o mesmo, eximindo o doador de toda e
+          qualquer responsabilidade por quaisquer atos praticados pelo e com o
+          animal a partir desta data, exceto quanto à castração se assim foi
+          pactuado. Declaro ainda, estar ciente de todos os cuidados que este
+          animal exige no que se refere à sua guarda e manutenção, além de
+          conhecer todos os riscos inerentes à espécie com o convívio com
+          humanos, estando apto a guardá-lo e vigiá-lo, comprometendo-me a
+          proporcionar-lhe boas condições de alojamento, alimentação, higiene,
+          conforto térmico, assim como espaço físico que possibilite ao animal
+          se exercitar; e, ainda, ambiente que lhe proporcione afeto e distância
+          de qualquer risco, crueldade, incluindo isolamento e abandono.
+          Responsabilizo-me por preservar a saúde e integridade do animal e a
+          submetê-lo aos cuidados médico-veterinários sempre que necessário para
+          este fim. Comprometo-me a não transmitir a posse deste animal a outrem
+          sem o conhecimento do doador, bem como a informá-lo de qualquer
+          situação de risco ou doença que o animal venha a se encontrar.
+          Comprometo-me, também, a permitir o acesso do doador, e a quem com ele
+          se encontrar, ao local de residência do animal, para averiguação de
+          suas condições. Tenho conhecimento de que, caso seja constatado, por
+          parte do doador ou qualquer pessoa ou órgão, que o animal se encontra
+          em situação inadequada para seu bem-estar, perderei a sua guarda, sem
+          prejuízo das penalidades legais, administrativas, cíveis e criminais.
+          Comprometo-me a cumprir toda a legislação vigente, municipal, estadual
+          e federal, relativa à posse de animais. Declaro-me, assim, ciente das
+          normas acima, as quais aceito, assumindo plenamente os deveres que
+          dele constam, bem como outros relacionados à posse responsável e que
+          não estejam incluídos neste Termo.
         </Text>
       </ScrollView>
 
